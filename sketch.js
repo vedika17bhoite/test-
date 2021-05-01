d6e81e236bde68a16214506ce5ec12f13cc61709
vedika17bhoite <vedika17bhoite@gmail.com>
2021-05-01 19:38:51 +0530

    change

---
+++ sketch.js
@@ -0,0 +1,61 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbin,ground	, paper;
+var world;
+
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paper=new paper(200,450,70);
+	ground= new ground(width/2,670,width,20);
+	dustbin=new dustbin(1200,650);
+
+	var render = Render.create( 
+		{
+			element : document.body,
+			engine: engine ,
+			options: {
+				width:1600,
+				height: 700,
+				wireframes: false}
+
+			
+		}
+	);
+
+
+	
+
+	Engine.run(engine);
+	Render.run(render);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+   paper.display();
+  ground.display();
+  dustbin.display();
+
+}
+
+function keyPressed() {
+
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+	}
+}
+
